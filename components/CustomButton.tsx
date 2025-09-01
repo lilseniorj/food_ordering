8d5b95597733ce66ba939305deb162faee68ca28
lil seniorj <Jesusvarguer18@gmail.com>
2025-09-01 15:02:39 -0500

    Auth Funcionality

---
+++ components/CustomButton.tsx
@@ -0,0 +1,29 @@
+import { CustomButtonProps } from '@/type'
+import React from 'react'
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native'
+import cn from "clsx";
+
+const CustomButton = ({
+  onPress,
+  title = 'Click Me',
+  style,
+  textStyle,
+  leftIcon,
+  isLoading = false,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity className={cn('custom-btn', style)} onPress={onPress} >
+      {leftIcon}
+
+      <View>
+        {isLoading ? (
+          <ActivityIndicator size="small" color="white" />
+        ): (
+          <Text className={cn('text-white-100 paragraph-semibold', textStyle)}>{title}</Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
