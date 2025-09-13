d2708e62ecc7d00c73aa87daaf765292447c479c
Duvan Vargas <duvandroid@gmail.com>
2025-09-12 19:08:34 -0500

    feat: progress

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,48 +1,51 @@
-import { FlatList, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import useAppwrite from '@/lib/useAppwrite'
-import { getCategories, getMenu } from '@/lib/appwrite'
-import { useLocalSearchParams } from 'expo-router'
-import { useEffect } from 'react'
-import CartButton from '@/components/CartButton'
-import cn from "clsx";
+import CartButton from "@/components/CartButton";
 import MenuCard from "@/components/MenuCard";
-import { MenuItem } from "@/type";
-import SearchBar from '@/components/SearchBar'
-import Filter from '@/components/Filter'
+import { getCategories, getMenu } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import cn from "clsx";
+import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { FlatList, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import Filter from "@/components/Filter";
+import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
-    const { category, query } = useLocalSearchParams<{query: string; category: string}>()
+  const { category, query } = useLocalSearchParams<{ query: string; category: string }>()
 
-    const { data, refetch, loading } = useAppwrite({ fn: getMenu, params: { category,  query,  limit: 6, } });
-    const { data: categories } = useAppwrite({ fn: getCategories });
+  const { data, refetch, loading } = useAppwrite({ fn: getMenu, params: { category, query, limit: 6, } });
+  const { data: categories } = useAppwrite({ fn: getCategories });
 
-    useEffect(() => {
-        refetch({ category, query, limit: 6})
-    }, [category, query ]);
+  useEffect(() => {
+    refetch({ category, query, limit: 6 })
+  }, [category, query]);
 
   return (
-    <SafeAreaView className='bg-white h-full'>
+    <SafeAreaView className="bg-white h-full">
       <FlatList
         data={data}
-        renderItem={({ item, index}) => {
+        renderItem={({ item, index }) => {
           const isFirstRightColItem = index % 2 === 0;
-        return (
-          <View className={cn("flex-1 max-w[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
-            <MenuCard item={item as MenuItem} />
-          </View>
-        )}}
+
+          return (
+            <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10' : 'mt-0')}>
+              {/* @ts-ignore */}
+              <MenuCard item={item} />
+            </View>
+          )
+        }}
         keyExtractor={item => item.$id}
         numColumns={2}
-        columnWrapperClassName='gap-7'
-        contentContainerClassName='gap-7 px-5 pb-32'
-        ListHeaderComponent={() =>(
-          <View className='my-5 gap-5'>
-            <View className='flex-between flex-row w-full'>
-              <View className='flex-start'>
-                <Text className='small-bold uppercase text-primary'>Search</Text>
-                <View className='flex-start flex-row gap-x-1 mt-0.5'>
-                  <Text className='paragraph-semibold text-dark-100'>Find you favorite food</Text>
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+          <View className="my-5 gap-5">
+            <View className="flex-between flex-row w-full">
+              <View className="flex-start">
+                <Text className="small-bold uppercase text-primary">Search</Text>
+                <View className="flex-start flex-row gap-x-1 mt-0.5">
+                  <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
                 </View>
               </View>
 
@@ -51,6 +54,7 @@ const Search = () => {
 
             <SearchBar />
 
+            {/* @ts-ignore */}
             <Filter categories={categories!} />
           </View>
         )}
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,74 +1,75 @@
-import { Account, Avatars, Client, Databases, ID, Query, Storage } from 'react-native-appwrite';
-import { CreateUserPrams, GetMenuParams, SignInParams} from "@/type";
+import { CreateUserPrams, GetMenuParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from "react-native-appwrite";
 
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   platform: "com.jsm.foodordering",
-  databaseId: "68af3d680002cea1d8ab",
-  bucketId: "68c1d3a10013bb19594e",
-  userCollectionId: "user",
-  categoriesCollectionId: "categories",
-  menuCollectionId: "menu",
-  customizationsCollectionId: "customizations",
-  menuCustomizationsCollectionId: "menu_customizations",
+  databaseId: '68629ae60038a7c61fe4',
+  bucketId: '68643e170015edaa95d7',
+  userCollectionId: '68629b0a003d27acb18f',
+  categoriesCollectionId: '68643a390017b239fa0f',
+  menuCollectionId: '68643ad80027ddb96920',
+  customizationsCollectionId: '68643c0300297e5abc95',
+  menuCustomizationsCollectionId: '68643cd8003580ecdd8f'
 }
 
 export const client = new Client();
 
 client
-  .setEndpoint(appwriteConfig.endpoint) // Your API Endpoint
-  .setProject(appwriteConfig.projectId) // Your project ID
-  .setPlatform(appwriteConfig.platform) // Your project ID
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform)
 
 export const account = new Account(client);
 export const databases = new Databases(client);
-export const storage = new Storage(client); // assuming you have a client instance
-const avatars = new Avatars(client)
+export const storage = new Storage(client);
+const avatars = new Avatars(client);
 
-export const createUser = async ({ email, password, name }: CreateUserPrams ) => {
-    try {
-        const newAccount = await account.create(ID.unique(), email, password, name)
-        if(!newAccount) throw Error;
+export const createUser = async ({ email, password, name }: CreateUserPrams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name)
+    if (!newAccount) throw Error;
 
-        await signIn({ email, password });
+    await signIn({ email, password });
 
-        const avatarUrl = avatars.getInitialsURL(name);
+    const avatarUrl = avatars.getInitialsURL(name);
 
-        return await databases.createDocument(
-          appwriteConfig.databaseId,
-          appwriteConfig.userCollectionId,
-          ID.unique(),
-          {  email, name, accountId: newAccount.$id, avatar: avatarUrl, }
-        );
-    } catch (e) {
-        throw new Error(e as string);
-    }
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+    );
+  } catch (e) {
+    throw new Error(e as string);
+  }
 }
 
 export const signIn = async ({ email, password }: SignInParams) => {
-    try {
-        const session = await account.createEmailPasswordSession(email, password);
-    } catch (e) {
-        throw new Error(e as string);
-    }
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+  } catch (e) {
+    throw new Error(e as string);
+  }
 }
 
 export const getCurrentUser = async () => {
   try {
     const currentAccount = await account.get();
-    if(!currentAccount) throw Error;
+    if (!currentAccount) throw Error;
 
     const currentUser = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.userCollectionId,
-      [Query.equal("accountId", currentAccount.$id)]
+      [Query.equal('accountId', currentAccount.$id)]
     )
 
-    if(!currentUser) throw Error;
+    if (!currentUser) throw Error;
 
     return currentUser.documents[0];
   } catch (e) {
+    console.log(e);
     throw new Error(e as string);
   }
 }
@@ -77,8 +78,8 @@ export const getMenu = async ({ category, query }: GetMenuParams) => {
   try {
     const queries: string[] = [];
 
-    if(category) queries.push(Query.equal("categories", category));
-    if(query) queries.push(Query.search("name", query));
+    if (category) queries.push(Query.equal('categories', category));
+    if (query) queries.push(Query.search('name', query));
 
     const menus = await databases.listDocuments(
       appwriteConfig.databaseId,
@@ -98,8 +99,9 @@ export const getCategories = async () => {
       appwriteConfig.databaseId,
       appwriteConfig.categoriesCollectionId,
     )
-return categories.documents;
+
+    return categories.documents;
   } catch (e) {
-      throw new Error(e as string);
+    throw new Error(e as string);
   }
 }
